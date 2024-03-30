ee97c3bd7b4b20564d9ba61b90e89cde19984cc0
Pratik Biswas <04biswaspratik@gmail.com>
2024-03-30 17:44:04 +0530

    o/p formatting using printf

--- Java/output_formatting.java
+++ Java/output_formatting.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class output_formatting {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    while (sc.hasNext()) {
+    System.out.println("================================");
+    while (sc.hasNext()) { // new thing
       String s = sc.next();
       int x = sc.nextInt();
 
-      // Format and print output
-      System.out.printf("%-15s%03d%n", s, x);
+      System.out.printf("%-15s%03d%n", s, x); // new thing
     }
-
+    System.out.println("================================");
     sc.close();
   }
 }
