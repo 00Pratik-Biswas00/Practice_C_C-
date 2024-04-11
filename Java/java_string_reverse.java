cd55d0785d4e5835d8986ac739b24592232685a0
Pratik Biswas <04biswaspratik@gmail.com>
2024-04-11 08:30:06 +0530

    java_string_reverse

---
+++ Java/java_string_reverse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class java_string_reverse {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine().toLowerCase();
+    int l = s.length();
+    String rev = "";
+
+    for (int i = l - 1; i >= 0; i--) {
+      rev = rev + s.charAt(i);
+    }
+    if (s.equals(rev))
+      System.out.println("Yes");
+
+    else
+      System.out.println("No");
+  }
+}
